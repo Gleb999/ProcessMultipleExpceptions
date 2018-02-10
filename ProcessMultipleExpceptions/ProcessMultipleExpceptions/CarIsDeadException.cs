b26012f625395aae0c7254d8e285991aad3c78d3
Gleb999 <lihachev99gleb@mail.ru>
2018-02-10 14:32:57 +0700

    Common catch statements

---
+++ ProcessMultipleExpceptions/ProcessMultipleExpceptions/CarIsDeadException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessMultipleExpceptions
+{
+    class CarIsDeadException : ApplicationException
+    {
+        public void Accelerate(int delta)
+        {
+            if (delta < 0)
+            {
+                throw new ArgumentOutOfRangeException("delta", "Speed must be qreater than zero!");
+            }
+            CarIsDeadException ex = new CarIsDeadException(string.Format("{0} has overheated!", PetName),
+                "You have a lead foot", DataTime.Now);
+            ex.HelpLink = "http://www.CarsRus.com";
+            throw ex;
+        }
+        private string messageDetails = String.Empty;
+
+        public DateTime ErrorTimeStamp { get; set; }
+        public string CauseOfError { get; set; }
+
+        public CarIsDeadException() { }
+        public CarIsDeadException(string message, string cause, DateTime time)
+        : base(message)
+        {
+            CauseOfError = cause;
+            ErrorTimeStamp = time;
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return string.Format("Car Error Messaqe: {0}", messageDetails);
+            }
+        }
+    }
+}
---
+++ ProcessMultipleExpceptions/ProcessMultipleExpceptions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessMultipleExpceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Custom Exceptions *****\n");
+            Car myCar = new Car("Rusty", 90);
+            try
+            {
+                myCar.Accelerate(90);
+            }
+
+            catch
+            {
+                Console.WriteLine("Something bad happened...");
+            }
+
+            /*catch (CarIsDeadException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }*/
+
+            Console.ReadLine();
+        }
+    }
+}
